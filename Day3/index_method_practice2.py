d015f15392fa5be51ea391112c47ab527f7b8e29
Ballarja <JamesBallard87@hotmail.com>
2023-11-02 12:46:54 -0500

    completed index_method_practice2.py and index_method_practice3.py

---
+++ Day3/index_method_practice2.py
@@ -0,0 +1,4 @@
+sentence = "In theory, theory and practice are the same. In practice, they are not."
+
+word = sentence.index("practice")
+print(word)
---
+++ Day3/index_method_practice3.py
@@ -0,0 +1,5 @@
+sentence = "In theory, theory and practice are the same. In practice, they are not."
+
+last_word = sentence.rindex("practice")
+
+print(last_word)
