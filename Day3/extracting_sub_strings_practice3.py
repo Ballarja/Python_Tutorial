34a34175b9a1acb7dd2e94e33083085987d0a40d
Ballarja <JamesBallard87@hotmail.com>
2023-11-02 13:32:48 -0500

    completed extracting_sub_strings_practice3.py

---
+++ Day3/extracting_sub_strings_practice3.py
@@ -0,0 +1,5 @@
+sentence = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+
+reversed_sentence = sentence[::-1]
+
+print(reversed_sentence)
