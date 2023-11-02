db8f44d7d3853ff2631c751620c8f4f7b77c7909
Ballarja <JamesBallard87@hotmail.com>
2023-11-02 11:20:37 -0500

    added round to day 2 challenge

--- Day2/day2_challenge.py
+++ Day2/day2_challenge.py
@@ -2,7 +2,7 @@ question_one = input("What is your name?")
 question_two = input("How much did you sale?")
 
 question_two = int(question_two)
-employee_commission = question_two * 13 / 100
+employee_commission = round(question_two * 13 / 100)
 
 print(question_one)
 print(question_two)
